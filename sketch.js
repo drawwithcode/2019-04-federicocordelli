9c5be2970e41d5093d609d7e885e8f744450058c
FedericoCordelli <37138962+FedericoCordelli@users.noreply.github.com>
2019-10-15 22:06:04 +0200

    consegna

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
+var mySong;
+var myImage;
+var analyzer;
+var secondImage;
+
+
 function preload(){
-  // put preload code here
+mySong = loadSound("./sounds/1.mp3");
+myImage = loadImage("./images/604.jpg");
+secondImage = loadImage("./images/486.jpg");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  background(255,0,0);
+  mySong.loop();
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
 }
 
 function draw() {
-  // put drawing code here
+  imageMode(CENTER);
+  image(myImage, width/2, height/2, myImage.width/4, myImage.height/4);
+  volume = analyzer.getLevel();
+  volume = map(volume, 0, 1, 0, height/5);
+  imageMode(CENTER);
+  image(secondImage, width/2, height/2, secondImage.width/(volume/4), secondImage.height/(volume/4));
+  mySong.play();
 }
